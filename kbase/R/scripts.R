101286e019693cac9fb64ce36ca7e81d9f7f0c12
heavyrick <heavyrick@gmail.com>
2019-09-24 12:32:58 -0300

    analise em R

--- kbase/R/scripts.R
+++ kbase/R/scripts.R
@@ -1,20 +1,48 @@
-setwd('C:\Users\ricardo\Desktop\ds\datascience\kbase\R')
+#setwd('C:\Users\ricardo\Desktop\ds\datascience\kbase\R') # windows
+setwd('/home/usuario/www/repositorios/datascience/kbase/R') # linux
 
-# Bibliotecas
-library(ggplot2)
-library(dplyr)
-library(maps)
-library(ggmap)
-library(mongolite)
-library(lubridate)
-library(gridExtra)
-require (sqldf)
+"
+Eleições Municipais 2016: Americana/SP - Vereadores
 
-# inserir collections nos dataframes
-col_partidos <- mongo("partidos", url = "mongodb://localhost:27017/eleicoes")
-df_partidos = col_partidos$find ('{}')
+Análise de dados das eleições de Americana/SP do ano 2016.
 
+> Objetivos:
+* Entender a distribuição de votos por partido nas seções;
+* Listar os partidos por votação;
+* Analisar o desempenho da esquerda na votação na cidade;
+"
 
+# Carregar dados
+source(file = "ler_dados.R", encoding = "UTF-8")
+
+##
 sqldf("SELECT * FROM df_partidos ORDER BY Legenda ASC")
+
+#================================================================#
+
+# Partidos e espectros políticos
+
+# 1) Qual a quantidade de votos por partido ?
+
+" 
+Os votos ficam na base `df_secao_votacao_num` e devem ser juntados  com a base `df_partidos`. Temos que retornar a quantidade de votos por 
+sigla partidária, não mostrando os que não tiveram votos.
+"
+
+df1 = sqldf("
+      SELECT p.Sigla, SUM(v.QT_VOTOS) as votos 
+      FROM df_partidos p
+      LEFT JOIN df_secao_votacao_mun v ON p.Legenda = v.NR_PARTIDO
+      GROUP BY p.Sigla
+      HAVING SUM(v.QT_VOTOS) > 0
+      ORDER BY votos DESC
+      ")
+
+
+# http://material.curso-r.com/ggplot/
+#ggplot(data = df1)
+
+
+
 
 
